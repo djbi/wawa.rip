eaca1c9e922c65c32c6420636d26f03e9f8454b3
Faith <faith694no@outlook.com>
2025-03-07 19:17:45 +0000

    Update logos.js

--- scripts/logos.js
+++ scripts/logos.js
@@ -16,11 +16,9 @@ function loadLogos() {
         img.src = imageSrc;
         img.alt = url.split('/')[2]; // Simplified alt text
         img.className = 'social-logo';
-        // Auto-scale image to fit 50x50 while maintaining aspect ratio
-        img.style.maxWidth = '50px';
-        img.style.maxHeight = '50px';
-        img.style.width = '100%';
-        img.style.height = 'auto';
+        // Auto-scale image to fit defined size while maintaining aspect ratio
+        img.style.maxWidth = '100%';
+        img.style.maxHeight = '100%';
         link.appendChild(img);
         socialIcons.appendChild(link);
     }
