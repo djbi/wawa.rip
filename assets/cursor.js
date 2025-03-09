82a9d1a51fc7d32c28a542e5a1b4e983f439f7ae
Faith <faith694no@outlook.com>
2025-03-09 17:09:19 +0000

    Update cursor.js

--- assets/cursor.js
+++ assets/cursor.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cursor = document.getElementById('custom-cursor');
-    if (!cursor) {
-        console.error('Custom cursor element not found');
-        return;
-    }
-
     document.addEventListener('mousemove', (e) => {
         cursor.style.left = `${e.pageX - 10}px`;
         cursor.style.top = `${e.pageY - 10}px`;
