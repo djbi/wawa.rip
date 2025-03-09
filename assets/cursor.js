8956113de66a39c9b6024048072ab7deefc2ee44
Faith <faith694no@outlook.com>
2025-03-09 17:46:35 +0000

    Delete assets directory

--- assets/cursor.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cursor = document.getElementById('custom-cursor');
-    document.addEventListener('mousemove', (e) => {
-        cursor.style.left = `${e.pageX - 10}px`;
-        cursor.style.top = `${e.pageY - 10}px`;
-    });
-});
